f10b31bf5338c3323c83189b432d10158dd59e1f
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-04-19 18:37:09 -0500

    Ya ejecuta comandos sin salir del proceso padre.

--- miniShell.c
+++ miniShell.c
@@ -13,65 +13,70 @@ int main()
     char commands[15][15]; //can store 15 commands of 15 characters
     int i,j,cnt;
  
-    printf("Enter a string: ");
-    fgets(str, 100, stdin);
  
-    j=0; cnt=0;
-    for(i=0;i<=(strlen(str));i++)
-    {
-        // if space or NULL found, assign NULL into commands[cnt]
-        if(str[i]==' '||str[i]=='\0')
-        {
-            commands[cnt][j]='\0';
-            cnt++;  //for next command
-            j=0;    //for next command, init index to 0
-        }
-        else
-        {
-            commands[cnt][j]=str[i];
-            j++;
-        }
-    }
-    printf("\nOriginal String is: %s",str);
-    printf("\nStrings (commands) after split by space:\n");
+    while(1){
+ 
+    	printf("Enter a string: ");
+    	fgets(str, 100, stdin);
+ 
+ 
+ 
+	j=0; cnt=0;
+    	for(i=0;i<=(strlen(str));i++)
+    	{
+        	// if space or NULL found, assign NULL into commands[cnt]
+        	if(str[i]==' '||str[i]=='\0' || str[i] == '\r' || str[i] == '$' || str[i] == '\n')
+        	{
+            		commands[cnt][j]='\0';
+            		cnt++;  //for next command
+            		j=0;    //for next command, init index to 0
+        	}
+        	else
+        	{
+            		commands[cnt][j]=str[i];
+            		j++;
+        	}
+    	}
+    	//printf("\nOriginal String is: %s",str);
+    	//printf("\nStrings (commands) after split by space:\n");
     
         
-    printf("\nNumber of commands: %d\n", cnt);
+    	//printf("\nNumber of commands: %d\n", cnt);
         
-    int argc;    
-    char *cmd[5];    
+    	int argc;    
+    	char *cmd[5];    
         
-    argc = 5;
+    	argc = 5;
     
-    for(i=0;i < cnt;i++){
-        printf("%s\n",commands[i]);
-    	cmd[i] = commands[i];
-    	printf("%s\n",cmd[i]);
-    }
-    cmd[i] = NULL;
-    printf("\nAfter for, i: %d\n", i);
+    	for(i=0;i < cnt;i++){
+       	//printf("%s\n",commands[i]);
+    		cmd[i] = commands[i];
+    		//printf("%s\n",cmd[i]);
+    	}
+    	cmd[i - 1] = NULL;
+    	//printf("\nAfter for, i: %d\n", i);
     
     
     
-    pid_t sonsPid;
-    sonsPid = fork();    
+    	pid_t sonsPid;
+    	sonsPid = fork();    
         
-    if (sonsPid < 0){
-    	//fork failed
-    	fprintf(stderr, "fork failed\n");
-    	exit(1);
-    } else if (sonsPid == 0){
-    	//child, commands will be executed here:
+    	if (sonsPid < 0){
+    		//fork failed
+    		fprintf(stderr, "fork failed\n");
+    		exit(1);
+    	} else if (sonsPid == 0){
+    		//child, commands will be executed here:
     	
-    	execvp(cmd[0], cmd); /*Sons process deads and is replaced by the commands execution*/
-    	printf("\nExecvp failed to execute.\n");
+    		execvp(cmd[0], cmd); /*Sons process deads and is replaced by the commands execution*/
+    		printf("\nExecvp failed to execute.\n");
     	
-    } else {
-	    //printf("\nDad is here\n");
+    	} else {
+	    printf("\nDad is here\n");
+    	}
+    
+    
     }
-    
-    
-    	
         
         
     return 0;
