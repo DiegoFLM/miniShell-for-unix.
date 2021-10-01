93135a42721047886db919883ac0ccc46994c7d7
DiegoF <diego.ledesma@correounivalle.edu.co>
2021-10-01 04:03:08 -0500

    Something to show

--- miniShell.c
+++ miniShell.c
@@ -77,37 +77,146 @@ void pipefunction(char *comand[], char *args[]){
 
 int main(void) {
   printf("**********MiniShell**********\n");
-  printf(">>>  ");
+
   char  entrada[100];//Se crea un array de cadenas de caracteres para almacenar la entrada
-  scanf(" %[^\n]", entrada);//Se pide la entrada al usuario, por teclado
-  /*Se busca en la cadena de entrada ya sea el simbolo de pipe o el de redireccionamiento para manejar cada caso por separado.
-  Este if se ejecuta si en la cadena se encuentra el simbolo de pipe*/
-  if( strstr(entrada, "|") != NULL ){
-   char *pipe[3];
-   split(entrada, pipe, "|");//Se separa la entrada en las dos subcadenas delimitadas por el simbolo|
+  
+  
+  int t = 1; 
+  int estado;
+
+  while(t){
+
+    memset(entrada, '\0', 100);
+
+
+    printf(">>>  ");
+  
+    scanf(" %[^\n]", entrada);//Se pide la entrada al usuario, por teclado
+    /*Se busca en la cadena de entrada ya sea el simbolo de pipe o el de redireccionamiento para manejar cada caso por separado.
+    Este if se ejecuta si en la cadena se encuentra el simbolo de pipe*/
+  
+    if( strstr(entrada, "exit") != NULL ){
+      t = 0;
+    }
+  
+    if( strstr(entrada, "|") != NULL ){
+      char *pipe[3];
+      split(entrada, pipe, "|");//Se separa la entrada en las dos subcadenas delimitadas por el simbolo|
                              //Cada una de ellas contiene un comando y los argumentos
-   char *firstComand[4];  
-   char *secondComand[4];
-   split(pipe[0], firstComand, " ");//Se separa la cadena por palabras
-   split(pipe[1], secondComand, " ");
-   pipefunction(firstComand, secondComand);
-  }
-  else if( strstr(entrada, ">") != NULL){//Se ejecuta si se encuentra el simbolo de direccionamiento
-   char *redirect[3];
-   split(entrada, redirect, ">");/*Se separa la entrada en dos subcadenas, una de ellas contiene un comando
+      char *firstComand[4];  
+      char *secondComand[4];
+      split(pipe[0], firstComand, " ");//Se separa la cadena por palabras
+      split(pipe[1], secondComand, " ");
+
+
+      //ADDED LINES
+      pid_t sonsPidPipe;
+      sonsPidPipe = fork();  
+
+      if (sonsPidPipe < 0){
+        //fork failed
+    		fprintf(stderr, "fork failed\n");
+    		exit(1);
+      }else if (sonsPidPipe == 0){
+        pipefunction(firstComand, secondComand);
+      }else{
+        //printf("\nDad is here\n");
+        wait(&estado);
+     		
+        if(WIFEXITED(estado)){
+    	    if(WEXITSTATUS(estado)){
+            printf("\n:(\n");
+    	    }else{
+            printf("\n:)\n");
+          }    	
+	      }
+      }
+
+
+
+
+    }else if( strstr(entrada, ">") != NULL){//Se ejecuta si se encuentra el simbolo de direccionamiento
+      char *redirect[3];
+      split(entrada, redirect, ">");/*Se separa la entrada en dos subcadenas, una de ellas contiene un comando
                                    junto con sus argumentos, la otra contiene el nombre del archivo  donde
                                    se va a redireccionar la salida*/
-   char *comand[4];
-   char *out[2];
-   out[0] = redirect[1];
-   out[1] = NULL;
-   split(redirect[0], comand, " ");
-   redirectfunction(comand, out);
-  }else{//Se ejecuta para cualquier comando distinto a casos anteriores
-    char *args[7];
-    split(entrada, args, " ");
-    execvp(args[0], args);
+      char *comand[4];
+      char *out[2];
+      out[0] = redirect[1];
+      out[1] = NULL;
+      split(redirect[0], comand, " ");
+
+
+      //ADDED LINES
+      pid_t sonsPidRed;
+      sonsPidRed = fork();  
+
+      if (sonsPidRed < 0){
+        //fork failed
+    		fprintf(stderr, "fork failed\n");
+    		exit(1);
+      }else if (sonsPidRed == 0){
+        redirectfunction(comand, out);
+      }else{
+
+        //printf("\nDad is here\n");
+        wait(&estado);
+     		
+        if(WIFEXITED(estado)){
+    	    if(WEXITSTATUS(estado)){
+            printf("\n:(\n");
+    	    }else{
+            printf("\n:)\n");
+          }    	
+	      }
+    
+      }
+
+      
+    
+    
+    
+    
+    
+    }else{//Se ejecuta para cualquier comando distinto a casos anteriores
+      char *args[7];
+      split(entrada, args, " ");
+
+
+      //ADDED LINES
+      pid_t sonsPidOthers;
+      sonsPidOthers = fork();  
+
+      if (sonsPidOthers < 0){
+        //fork failed
+    		fprintf(stderr, "fork failed\n");
+    		exit(1);
+      }else if (sonsPidOthers == 0){
+        execvp(args[0], args);
+      }else{
+
+        //printf("\nDad is here\n");
+        wait(&estado);
+     		
+        if(WIFEXITED(estado)){
+    	    if(WEXITSTATUS(estado)){
+            printf("\n:(\n");
+    	    }else{
+            printf("\n:)\n");
+          }    	
+	      }
+    
+      }
+
+
+
+
+
+    }
+
   }
+
+
   return 0;
 }
 
